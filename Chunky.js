9604aeb2636e54aeb54f62753d814617c23535e4
Shawn Whinnery <shawnsdrive@gmail.com>
2019-07-12 00:04:21 -0700

    v1

--- Chunky.js
+++ Chunky.js
@@ -1,48 +1,84 @@
 // chunky async iterators
-var _chunk = 500
-var _wait = 0
+var _chunk = 500,
+    _wait = 0
+
+function walk({ array, chunk = _chunk, wait = _wait, onStep }) {
+    return new Promise((resolve, reject) => {
+
+        var index = 0,
+            step = () => {
+                var start = index,
+                    end = Math.min(array.length, start + chunk),
+                    complete = () => { end = array.length }
+
+                for (let i = start; i < end; i++) {
+                    onStep(i, complete)
+                }
+
+                index = end
+
+                if (end < array.length) {
+                    return setTimeout(step, wait)
+                }
+
+                resolve()
+
+            }
+
+        step()
+    })
+}
+
 const Chunky = {
     map: async (array, fn, chunk = _chunk, wait = _wait) => {
-        var index = 0,
-            newArray = [],
-            step = () => {
-                var start = index,
-                    end = Math.min(array.length, start + chunk)
-
-                for (let i = start; i < end; i++) {
-                    newArray.push(fn(array[i], i))
-                }
-
-                index = end
-
-                if (end < array.length) {
-                    setTimeout(step, wait)
-                } else {
-                    Promise.resolve(newArray)
-                }
+        var newArray = []
+        await walk({
+            array,
+            chunk,
+            wait,
+            onStep: (i) => {
+                newArray.push(fn(array[i], i))
             }
-
-        step()
+        })
+        return newArray
     },
     forEach: async (array, fn, chunk = _chunk, wait = _wait) => {
-        var index = 0,
-            step = () => {
-                var start = index,
-                    end = Math.min(array.length, start + chunk)
-
-                for (let i = start; i < end; i++) {
-                    fn(array[i], i)
+        return await walk({
+            array,
+            chunk,
+            wait,
+            onStep: i => fn(array[i], i)
+        })
+    },
+    some: async (array, fn, chunk = _chunk, wait = _wait) => {
+        var one = false
+        await walk({
+            array,
+            chunk,
+            wait,
+            onStep: (i, end) => {
+                if (fn(array[i], i)) {
+                    one = true
+                    end()
                 }
 
-                index = end
-
-                if (end < array.length) {
-                    setTimeout(step, wait)
-                } else {
-                    Promise.resolve()
-                }
             }
-
-        step()
+        })
+        return one
+    },
+    filter: async (array, fn, chunk = _chunk, wait = _wait) => {
+        var newArray = []
+        await walk({
+            array,
+            chunk,
+            wait,
+            onStep: (i) => {
+                if (fn(array[i], i)) newArray.push(array[i])
+            }
+        })
+        return newArray
     }
 }
+
+
+module.exports = Chunky
---
+++ test.js
@@ -0,0 +1,53 @@
+var Chunky = require('./Chunky.js'),
+    testData = [],
+    arraysMatch = (a, b) => {
+        return !a.some((n, i) => { return n !== b[i] })
+    }
+
+while (testData.length < 100000) {
+    testData.push(Math.round(Math.random() * 1000))
+}
+
+async function test() {
+
+    try {
+
+        var filter = (number, i) => {
+            return number > 500
+        },
+        chunk = 100000,
+            chunkyFilter = await Chunky.filter(testData, filter, chunk),
+            nativeFilter = testData.filter(filter),
+            filtersMatch = arraysMatch(chunkyFilter, nativeFilter),
+            map = (number, i) => {
+                return number * -1
+            },
+            chunkyMap = await Chunky.map(testData, map, chunk),
+            nativeMap = testData.map(map),
+            mapsMatch = arraysMatch(chunkyMap, nativeMap),
+            some = (number, i) => {
+                return (
+                    i === 420 && number === testData[420]
+                )
+            },
+            chunkySome = await Chunky.some(testData, some, chunk),
+            nativeSome = testData.some(some),
+            somesMatch = chunkySome === nativeSome
+
+        return (
+            filtersMatch && mapsMatch && somesMatch && chunkySome === true
+        )
+
+    } catch (error) {
+
+        Promise.reject(error)
+
+    }
+
+}
+var start = Date.now()
+test().then(success => {
+    console.log('pass: '+success+' ('+(Date.now() - start)+'ms)')
+}).catch(error => {
+    console.log('error: '+error+' ('+(Date.now() - start)+'ms)')
+})
